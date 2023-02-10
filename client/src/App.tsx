e3972b2c4705bd59a5e7c2de68ced633f713ceca
Vinicius  Prates <viniprates2211@gmail.com>
2023-02-10 19:37:47 +0000

    setup front-end using vite+ react

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>PRT NOTES</h1>
+      
+    </div>
+  )
+}
+
+export default App
