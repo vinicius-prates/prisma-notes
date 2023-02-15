0f1916d047182533f12e83ab50b47aae5d913b92
vinicius-prates <viniprates2211@gmail.com>
2023-02-15 20:22:37 -0300

    style: styled map note

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,27 +12,28 @@ function App() {
 
   return (
     <div className="flex flex-col  w-screen min-h-screen bg-[#1A120B]">
-      <div className="flex flex-row gap-1 my-10 justify-evenly lg:mx-96 mx-10">
+      <div className="flex flex-row gap-1 my-10 justify-evenly lg:mx-96 mx-4">
         <div className="flex flex-row gap-2 items-center self-center">
-          <h1 className="text-2xl font-bold text-[whitesmoke]"> Made by:</h1>
-          <a href="https://github.com/vinicius-prates" target="_blank" className="text-2xl font-bold text-[#D5CEA3]">vinicius-prates</a>
+          <h1 className="lg:text-2xl  font-bold text-[whitesmoke]"> Made by:</h1>
+          <a href="https://github.com/vinicius-prates" target="_blank" className="lg:text-2xl font-bold text-[#D5CEA3]">vinicius-prates</a>
         </div>
         <div className="flex flex-row gap-2 items-center self-center">
 
-        <h1 className="text-6xl font-bold text-[#D5CEA3]">prt</h1>
-        <h1 className="text-6xl text-[whitesmoke] font-bold">notes</h1>
+        <h1 className="lg:text-6xl text-4xl font-bold text-[#D5CEA3]">prt</h1>
+        <h1 className="lg:text-6xl text-4xl text-[whitesmoke] font-bold">notes</h1>
         </div>
 
       </div>
 
       <div>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center mx-5 lg:mx-0">
         {data?.allNotes.map((note, key) => (
           <div key={key} className="flex flex-col lg:w-[40rem] bg-[#3C2A21] px-4 py-2 rounded-md">
 
             <h1 className="text-[#D5CEA3] text-2xl font-bold ">{note.title}</h1>
             <p className="text-[whitesmoke] text-lg">{note.note}</p>
-            <div className=""></div>
+            
+            <div className="text-[#D5CEA3] text-sm italic opacity-50 text-right">{note.createdAt.toString()}</div>
           </div>
         ))}
         </div>
