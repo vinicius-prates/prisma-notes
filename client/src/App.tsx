f2a39fdee2c1b8e46fddb37e607760179612747a
vinicius-prates <viniprates2211@gmail.com>
2023-02-15 19:55:33 -0300

    style: app page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,30 @@
-import { useState } from 'react'
-import { useQuery, useQueryClient } from 'react-query';
-import './App.css'
-import { api } from './util/trpc';
+import { useState } from "react";
+import { useQuery, useQueryClient } from "react-query";
+import "./App.css";
+import { api } from "./util/trpc";
 
 function App() {
-    const queryClient = useQueryClient();
-    const { data } = useQuery(["getNotes"], () => api.note.allNotes.query());
+  const queryClient = useQueryClient();
+  const { data, isLoading, error } = useQuery(["getNotes"], () =>
+    api.note.allNotes.query()
+  );
 
   return (
-    <div className="App">
-      <h1>{data?.allNotes.map((note, key ) => (
-          <div key={key}>
-            <h1>{note.title}</h1>
-          </div>
-  
-      ))}</h1>
-    <div>
-      <button className='px-6 py-4 bg-slate-600 text-white rounded-xl' onClick={async () => {
-         await api.note.createNote.mutate({
-          title: 'lero',
-          note: ' KKKKKKKKKKKKKK'
-        })
-      }}>lero</button>  
+    <div className="flex flex-col  w-screen min-h-screen bg-[#1A120B]">
+      <div className="flex flex-row gap-1 my-10 justify-evenly lg:mx-96 mx-10">
+        <div className="flex flex-row gap-2 items-center self-center">
+          <h1 className="text-2xl font-bold text-[whitesmoke]"> Made by:</h1>
+          <a href="https://github.com/vinicius-prates" target="_blank" className="text-2xl font-bold text-[#D5CEA3]">vinicius-prates</a>
+        </div>
+        <div className="flex flex-row gap-2 items-center self-center">
+
+        <h1 className="text-6xl font-bold text-[#D5CEA3]">prt</h1>
+        <h1 className="text-6xl text-[whitesmoke] font-bold">notes</h1>
+        </div>
+
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
