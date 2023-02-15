05fa6502ea7cd2719afde8124a2ff0f0ea63fab5
vinicius-prates <viniprates2211@gmail.com>
2023-02-15 20:50:41 -0300

    styled and format

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,7 +10,6 @@ function App() {
   );
   const [showModal, setShowModal] = useState(false);
 
-
   return (
     <div className="flex flex-col  w-screen min-h-screen bg-[#1A120B]">
       <div className="flex flex-row gap-1 my-10 justify-evenly lg:mx-96 mx-4">
@@ -33,64 +32,61 @@ function App() {
             notes
           </h1>
         </div>
-        
       </div>
 
       <div className="flex items-center justify-center ">
-      <button
-        className="bg-[#E5E5CB] text-[#1A120B] active:bg-[#3C2A21] font-bold w-60 lg:w-[40rem] uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none my-10 ease-linear transition-all duration-150"
-        type="button"
-        onClick={() => setShowModal(true)}
-      >
-        New note
-      </button>
-      {showModal ? (
-        <>
-          <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-          >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-[#3C2A21] outline-none focus:outline-none">
-                <div className="flex items-start justify-between p-5  rounded-t">
-                  <h3 className="text-3xl font-bold text-[#D5CEA3]">
-                    New Note
-                  </h3>
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 font-bold text-[#D5CEA3] opacity-5 float-right text-3xl leading-none  outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
-                  >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      close
-                    </span>
-                  </button>
-                </div>
-                <div className="flex flex-col gap-2 mx-4 items-center">
-                  <input className="bg-[#D5CEA3] focus:outline-none text-[#1A120B] w-52 font-bold px-1 py-2 rounded-md"></input>
-                  <textarea className="bg-[#D5CEA3] focus:outline-none text-[#1A120B] w-52 h-52 font-bold px-1 py-2 resize-none rounded-md"></textarea>
-                </div>
-               
-                <div className="flex items-center justify-end p-6 rounded-b">
-                  <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Close
-                  </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Add Note
-                  </button>
+        <button
+          className="bg-[#E5E5CB] text-[#1A120B] active:bg-[#3C2A21] font-bold w-60 lg:w-[40rem] uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none my-10 ease-linear transition-all duration-150"
+          type="button"
+          onClick={() => setShowModal(true)}
+        >
+          New note
+        </button>
+        {showModal ? (
+          <>
+            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+              <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-[#3C2A21] outline-none focus:outline-none">
+                  <div className="flex items-start justify-between p-5  rounded-t">
+                    <h3 className="text-3xl font-bold text-[#D5CEA3]">
+                      New Note
+                    </h3>
+                    <button
+                      className="p-1 ml-auto bg-transparent border-0 font-bold text-[#D5CEA3] opacity-5 float-right text-3xl leading-none  outline-none focus:outline-none"
+                      onClick={() => setShowModal(false)}
+                    >
+                      <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                        close
+                      </span>
+                    </button>
+                  </div>
+                  <div className="flex flex-col gap-2 mx-4 items-center">
+                    <input className="bg-[#E5E5CB] focus:outline-none text-[#1A120B] w-64 lg:w-[40rem] font-bold px-1 py-2 rounded-md"></input>
+                    <textarea className="bg-[#E5E5CB] focus:outline-none text-[#1A120B] w-64 lg:w-[40rem] h-52 lg:h-96 font-bold px-1 py-2 resize-none rounded-md"></textarea>
+                  </div>
+
+                  <div className="flex items-center justify-end p-6 rounded-b">
+                    <button
+                      className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      type="button"
+                      onClick={() => setShowModal(false)}
+                    >
+                      Close
+                    </button>
+                    <button
+                      className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      type="button"
+                      onClick={() => setShowModal(false)}
+                    >
+                      Add Note
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
-      ) : null}
+            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+          </>
+        ) : null}
       </div>
 
       <div>
