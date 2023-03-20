9f61643f9bc3d4c63b29c9052a229d9f6c158e9c
vinicius-prates <viniprates2211@gmail.com>
2023-03-20 15:46:03 -0300

    feat: react router, page by note id

---
+++ client/src/components/[id].tsx
@@ -0,0 +1,11 @@
+import { useParams } from "react-router-dom"
+
+export const NotePage = () => {
+
+    const { id } = useParams();
+    return(
+        <div>
+            
+        </div>
+    )
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import App from './App'
+import  { createBrowserRouter , RouterProvider } from "react-router-dom"
+import { NotePage } from './components/[id]'
 
 const queryClient = new QueryClient()
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: `/note/:id`,
+    element: <NotePage/> 
+  }
+])
+
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 
   <QueryClientProvider client={queryClient}>
-    <App />
+    <RouterProvider router={router}/>
     </QueryClientProvider>
 )
