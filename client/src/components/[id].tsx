1c872b88536360d200b9b300251000bd1df59d87
vinicius-prates <viniprates2211@gmail.com>
2023-03-20 16:55:31 -0300

    style

--- client/src/components/[id].tsx
+++ client/src/components/[id].tsx
@@ -9,7 +9,7 @@ export const NotePage = () => {
     return(
         <div className="h-screen bg-[#1A120B]">
             <NavBar/>
-            <div className="bg-[#3C2A21] p-4 my-5 mx-52 rounded">
+            <div className="bg-[#3C2A21] p-4 my-5 mx-96 rounded">
                 <h1 className="text-[#D5CEA3] font-bold text-2xl">{data?.title}</h1>
                 <p className="text-[whitesmoke] text-lg">{data?.note}</p>
             </div>
