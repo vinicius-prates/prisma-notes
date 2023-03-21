c85f39a302514afe7db2bd086f18162eedc991e7
vinicius-prates <viniprates2211@gmail.com>
2023-03-20 21:25:38 -0300

    feat: note date

--- client/src/components/[id].tsx
+++ client/src/components/[id].tsx
@@ -1,19 +1,26 @@
 import { useQuery } from "react-query";
-import { useParams } from "react-router-dom"
-import { api } from "../util/trpc"
+import { useParams } from "react-router-dom";
+import { api } from "../util/trpc";
 import { NavBar } from "./NavBar";
 export const NotePage = () => {
-
-    const { id } = useParams();
-    const {data} = useQuery(['GetNote'], ()=> api.note.singleNote.query({id}))
-    return(
-        <div className="h-screen bg-[#1A120B]">
-            <NavBar/>
-            <div className="bg-[#3C2A21] p-4 my-5 mx-96 rounded">
-                <h1 className="text-[#D5CEA3] font-bold text-2xl">{data?.title}</h1>
-                <p className="text-[whitesmoke] text-lg">{data?.note}</p>
-            </div>
-            
+  const { id } = useParams();
+  const { data } = useQuery(["GetNote"], () =>
+    api.note.singleNote.query({ id })
+  );
+  return (
+    <div className="flex flex-col bg-[#1A120B]">
+      <NavBar />
+      <div className="h-screen bg-[#1A120B] flex flex-col justify-between">
+        <div className="bg-[#3C2A21] p-4 my-5 mx-96 rounded">
+          <h1 className="text-[#D5CEA3] font-bold text-2xl">{data?.title}</h1>
+          <p className="text-[whitesmoke] text-lg">{data?.note}</p>
         </div>
-    )
-}
+        <div className="items-center text-center">
+          <p className="text-[#D5CEA3] text-lg opacity-60 py-10 font-bold italic">
+            {data?.createdAt.toString()}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
